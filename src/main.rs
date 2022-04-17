e9a5e102aae07efbeae187c562aad13d8a65fd2b
MisileLaboratory <misileminecord@gmail.com>
2022-04-17 22:16:12 +0900

    feat: :sparkles: simple random generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::{self, Rng};
+
 fn main() {
-  println!("hello, miku!")
+  let minimum = std::env::args().nth(1).expect("no min number").parse::<i128>().expect("no number");
+  let maximum = std::env::args().nth(2).expect("no max number").parse::<i128>().expect("no number");
+  if minimum >= maximum {
+    panic!("max number must be greater than min")
+  } else {
+    let mut rng = rand::thread_rng();
+    let number: i128 = rng.gen_range(minimum..maximum);
+    println!("number is {:?}", number);
+  }
 }
