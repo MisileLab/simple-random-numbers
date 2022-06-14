d1c638af4ade942deba7cd8b6a6d9035482c66c9
MisileLaboratory <misileminecord@gmail.com>
2022-06-14 22:14:23 +0900

    fix: :bug: fix deprecated warning

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ fn main() {
       .help("maximum number")
       .long("max"))
     .get_matches();
-  let minimum = app.value_of("min").expect("No minimum value").parse::<i128>().expect("Invalid minimum number");
-  let maximum = app.value_of("max").expect("No maximum value").parse::<i128>().expect("Invalid maximum number");
+  let minimum = app.get_one::<i128>("min").expect("No minimum value");
+  let maximum = app.get_one::<i128>("max").expect("No maximum value");
 
   if minimum >= maximum {
     panic!("max number must be greater than min")
   } else {
     let mut rng = rand::thread_rng();
-    let number: i128 = rng.gen_range(minimum..maximum);
+    let number: i128 = rng.gen_range(*minimum..*maximum);
     println!("number is {:?}", number);
   }
 }
